fd32b6fe8f7fd05565ea13ad6bb6defc1388ac5f
Syogo-Suganoya <registration@ssuganoya.com>
2026-03-02 12:12:18 +0900

    fix: 不要ファイル削除

--- debug_datasources.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from '@notionhq/client';
-const notion = new Client({ auth: 'abc' });
-console.log('notion.dataSources:', typeof (notion as any).dataSources);
-if ((notion as any).dataSources) {
-    console.log('notion.dataSources keys:', Object.keys((notion as any).dataSources));
-}
